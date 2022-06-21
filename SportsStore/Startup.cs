2171d369983a08199894fc5556f788070ee5cc32
n01451197 <n01451197@CISVM-RISA>
2022-06-21 11:03:17 -0400

    Add project files.

---
+++ SportsStore/Startup.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        IConfigurationRoot Configuration;
+        public Startup(IWebHostEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build();
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This was for establishing a fake repository before a true DB could be added
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
+
+            services.AddIdentity<AppUser, IdentityRole<Guid>>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false).AddNewtonsoftJson();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //details on page 12 of example 2
+            //Disable this method call in the launched product
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseMvc(routes => {
+                    routes.MapRoute(
+                            name: null,
+                            template:"{category}/Page{page:int}", 
+                            defaults: new { Controller = "Product", action = "List"});
+                    routes.MapRoute(
+                            name: null,
+                            template: "Page{page:int}",
+                            defaults: new { Controller = "Product", action = "List" });
+                    routes.MapRoute(
+                            name: null,
+                            template: "{category}",
+                            defaults: new { Controller = "Product", action = "List" });
+                    routes.MapRoute(
+                            name: null,
+                            template: "",
+                            defaults: new { Controller = "Product", action = "List" });
+                    routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+            });
+        }
+    }
+}
