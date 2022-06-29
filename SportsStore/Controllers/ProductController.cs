0eea5214c6e48d55e39276760bc06267668f1d16
Nicholas Duncan <n01451197@unf.edu>
2022-06-29 12:15:56 -0400

    Initial work in getting search functionality established. Eliminated a pesky ; in one of the views

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -16,6 +16,7 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
+
         public ViewResult List(string category, int page = 1) => View(new ProductListViewModel
         {
             Products = repository.Products.Where(p => category == null||p.Category == category).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
@@ -31,5 +32,22 @@ namespace SportsStore.Controllers
         });
 
         //Needs a method here to display manual product searching
+        public ViewResult Search(string name, int page = 1) => View(new ProductListViewModel
+        {
+            //A view is created that displayes all products that match the name
+            //The total products selected is determined by if a given product matches the query name, these are then ordered by their ID
+            //Then they are placed onto the page for the user to view
+            Products = repository.Products.Where(p => name == null || p.Name == name).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = name == null ?
+                repository.Products.Count() :
+                repository.Products.Where(e => e.Name == name).Count()
+            },
+            //Hujack the Current Category component of the ProductListViewModel for use in searching
+            CurrentCategory = name
+        });
     }
 }
